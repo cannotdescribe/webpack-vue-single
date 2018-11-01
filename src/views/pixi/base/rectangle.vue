93f24d79021847867336db573f29fea5ae2fe074
cannotdescribe <952376923@qq.com>
2018-11-02 01:11:59 +0800

    完整的开发生产环境

--- src/views/pixi/base/rectangle.vue
+++ src/views/pixi/base/rectangle.vue
@@ -5,15 +5,37 @@
 </template>
 
 <script>
-    import PIXI from "pixi.js"
+    import * as PIXI from 'pixi.js'
 
     export default {
+        data(){
+            return {
+                app:{},
+                sprite: {}
+            }
+        },
         mounted(){
-            let app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
-            this.$el.appendChild(app.view);
+            this.app = new PIXI.Application(800, 600, {backgroundColor : 0x1099bb});
+            this.$el.appendChild(this.app.view);
 
-            let bunny = PIXI.Sprite.fromImage('static/pixi/pony.png')
-            app.stage.addChild(bunny);
+            this.initSprite();
+        },
+        methods:{
+            initSprite(){
+                this.sprite = PIXI.Container();
+
+                for(let i=0;i<3;i++){
+                    let bunny = PIXI.Sprite.fromImage('static/pixi/pony.png')
+                    bunny.interactive = true;
+                    bunny.width = 50;
+                    bunny.height = 60;
+                    bunny.anchor.set(0.5)
+                    bunny.x = i*90;
+                    bunny.y = i*90;
+                    this.sprite.addChild(bunny);
+                }
+                this.stage.addChild(this.sprite);
+            }
         }
     }
 </script>
